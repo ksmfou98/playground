8bcc408c3d2e0a0ce3bc4fe46437ac5c84951dc8
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-03-10 22:15:19 +0900

    make server

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
+
+const handleHome = (req, res) => {
+  //req : 나에게 요청온거
+  res.send("Hello from home"); //res : 페이지로 보여주는거
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+};
+
+app.get("/", handleHome); // "/" 페이지에 접근하면 handleHome 함수 실행
+app.get("/profile", handleProfile); // "/profile" 페이지에 접근하면 handleProfile 함수 실행
+
+app.listen(PORT, handleListening); //npm start 했을때 PORT 로 서버를 열고 handleListening 함수 실행
