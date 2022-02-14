12e026408203c558e667d0db551af971b4c2e7d7
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-14 14:04:12 +0900

    feat: react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import Button from "components/Button";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -14,8 +14,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>테스트 입니다.</title>
       </Head>
       <QueryClientProvider client={queryClient}>
-        <Button />
-        <Component {...pageProps} />
+        <Hydrate state={pageProps.dehydratedState}>
+          <Button />
+          <Component {...pageProps} />
+        </Hydrate>
       </QueryClientProvider>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,21 @@ import Button from "components/Button";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import useTestQuery from "hooks/useQuery";
-import { dehydrate, QueryClient } from "react-query";
+import { dehydrate, QueryClient, useQuery } from "react-query";
 import { getTestList } from "api";
 import { useEffect } from "react";
 
-const Home: NextPage = () => {
-  const { data, isLoading } = useTestQuery();
-  console.log(data);
+const Home: NextPage = (props: any) => {
+  // const { data } = useTestQuery();
+  // console.log(data);
 
+  const { data, isLoading } = useQuery("test", getTestList, {
+    initialData: props.data,
+    cacheTime: 24 * 60 * 60 * 1000,
+    staleTime: 24 * 60 * 60 * 1000,
+  });
+
+  console.log(data);
   if (!data) return null;
 
   return (
@@ -74,11 +81,22 @@ const Home: NextPage = () => {
   );
 };
 
+export const getServerSideProps = async () => {
+  const data = await getTestList();
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
 // export const getServerSideProps = async () => {
-//   const data = await getTestList();
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery("testList", getTestList);
+
 //   return {
 //     props: {
-//       data,
+//       dehydratedState: dehydrate(queryClient),
 //     },
 //   };
 // };
