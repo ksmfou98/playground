07507c95ff75c783f77f734a646268b5c6004823
이도현 <64634992+ksmfou98@users.noreply.github.com>
2022-02-21 13:36:01 +0900

    feat: vanillajs webpack 셋업

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const dirname = path.resolve();
+
+const module = (env) => {
+  const isDevelopment = env === "isDevelopment";
+
+  return {
+    mode: isDevelopment ? "development" : "production",
+    devtool: isDevelopment ? "inline-source-map" : "hidden-source-map",
+    entry: "./src/main.js",
+    output: {
+      filename: "bundle.[fullhash].js",
+      path: path.resolve(dirname, "dist"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          use: {
+            loader: "url-loader",
+          },
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.js?$/,
+          include: path.resolve("src"),
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: {
+                minimize: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "style.css",
+      }),
+    ],
+
+    devServer: {
+      host: "localhost",
+      port: 8080,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
+};
+
+export default module;
